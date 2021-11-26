c2a44bea625e505cef9c65a5d4aa2bb1271ef5d2
Skrypkin Yevhenii <skrypkin.e@gmail.com>
2021-11-26 18:55:28 +0200

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function sum(a, b) {
-    return a + b
+  return a + b;
 }
 
-function sub (a, b) {
-    return a - b
-
-
-
-    
+function sub(a, b) {
+  return a - b;
 }
 
-function mul (a, b) {
-    return a * b
+function mul(a, b) {
+  return a * b;
 }
+
+sum(2, 2);
+sub(2, 2);
+mul(2, 2);
